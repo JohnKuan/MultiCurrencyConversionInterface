5317f98a66d8c33186fa5c11499f222866b87b7e
JohnKuan <founders2008@hotmail.com>
2020-05-27 19:33:14 +0800

    add Colors Constant

---
+++ MultiCurrencyConversionInterface/Misc/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  MultiCurrencyConversionInterface
+//
+//  Created by John Kuan on 27/5/20.
+//  Copyright © 2020 JohnKuan. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+public struct ColorManager {
+    // create static variables for custom colors
+    static let coreUIColor = Color("CoreUIColor")
+    static let coreUISecondaryColor = Color("CoreUISecondaryColor")
+}
